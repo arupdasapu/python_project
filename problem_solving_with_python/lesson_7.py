9d063889ba926620b2e84da79528227055cb1b2a
Arup Das Apu <127678412+arupdasapu@users.noreply.github.com>
2025-11-17 00:32:53 +0600

    Add files via upload

---
+++ problem_solving_with_python/lesson_7.py
@@ -0,0 +1,24 @@
+# Store course name
+
+course_name = 'Learn Python Programming'
+
+# Print the length (number of characters) of course_name
+print(len(course_name))
+
+# Print course_name in all UPPERCASE letters
+print(course_name.upper())
+
+# Print course_name in all lowercase letters
+print(course_name.lower())
+
+# Print course_name
+print(course_name)
+
+# Find the starting position (index) of the word 'Python'
+print(course_name.find('Python'))
+
+# Replace 'Learn' with 'Master' in course_name
+print(course_name.replace('Learn', 'Master'))
+
+# Check if the word 'learn' is inside course_name (will give True or False)
+print('learn' in course_name)
