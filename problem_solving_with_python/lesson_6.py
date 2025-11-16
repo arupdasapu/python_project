def77733a953a633656b18f89dc4003853bdeb74
Arup Das Apu <127678412+arupdasapu@users.noreply.github.com>
2025-11-17 00:32:05 +0600

    Add files via upload

---
+++ problem_solving_with_python/lesson_6.py
@@ -0,0 +1,19 @@
+# Store the name
+
+
+name = 'Arup Das Apu'
+
+# Store the code name
+code_name = 'ADA'
+
+# Create a message by joining text using +
+message = name + '(' + code_name + ') learner AI'
+
+# Print the first message
+print(message)
+
+# Create the same message using f-string (easier and cleaner)
+message_1 = f'{name}({code_name}) teaches AI'
+
+# Print the second message
+print(message_1)
