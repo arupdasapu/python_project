a83cd1b47758a70cf7048c661ca0d4d01a452b98
Arup Das Apu <127678412+arupdasapu@users.noreply.github.com>
2025-11-16 16:00:30 +0600

    Add files via upload

---
+++ problem_solving_with_python/lesson_1.py
@@ -0,0 +1,7 @@
+#print output
+print('This is our first python code')
+
+print("Hello Python")
+print("Now we can print outputs.")
+
+print('*' * 20)  # Print a line made of 20 stars
---
+++ problem_solving_with_python/lesson_2.py
@@ -0,0 +1,59 @@
+# print output & uned input function
+
+# Store age in a variable
+age = 30
+# Update the age
+age = 20
+
+# Store weight in a variable
+weight = 72.5
+
+# Store name in a variable
+name = "Arup Das Apu"
+
+# Store if good programmer or not (True/False)
+good_programmer = True
+
+# Print the value of age
+print(age)
+
+# Print the word 'age' (not the value)
+print('age')
+
+# Print the value of weight
+print(weight)
+
+# Print the value of name
+print(name)
+
+# Print if good programmer is True or False
+print(good_programmer)
+
+# --- Another Example ---
+
+# Store product name
+product_name = "Coffee"
+
+# Store product price
+price = 5.0
+
+# Store expiry date
+expiry_date = "1 January 2030"
+
+# Store if discount is available (True/False)
+discount_available = True
+
+# Print product name
+print(product_name)
+
+# Print product price
+print(price)
+
+# Print expiry date
+print(expiry_date)
+
+# Print if discount is available
+print(discount_available)
+
+# Ask user for their name
+input('What is your name?')
