6d51b5dc0dfca68ff9c71f15f5eac33c889d5e39
Arup Das Apu <127678412+arupdasapu@users.noreply.github.com>
2025-11-27 00:06:37 +0600

    Add files via upload

---
+++ problem_solving_with_python/lesson_12.py
@@ -0,0 +1,19 @@
+# Define the price of the item
+
+price = 9
+
+# Check if price is exactly 10
+if price == 10:
+    print("We will buy it")
+
+# Check if price is not equal to 10
+if price != 10:
+    print("The price is not 10")
+
+# Check if price is greater than 10
+if price > 10:
+    print("It is expensive")
+
+# Check if price is less than or equal to 10
+if price <= 10:
+    print("It is cheap")
