4972c17015a2b2f99a60f3febe0f0cf930228eae
Arup Das Apu <127678412+arupdasapu@users.noreply.github.com>
2025-12-15 23:53:58 +0600

    Implement search algorithm classes in Maze.py
    Added Node and StackFrontier classes for search algorithms.

--- search algorithm/Maze.py
+++ search algorithm/Maze.py
@@ -1,1 +1,37 @@
 
+# Search Algorithm
+
+import sys
+
+# Represents a node in the search tree
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state      # Current position (row, col)
+        self.parent = parent    # Node we came from
+        self.action = action    # Action taken to reach this node
+
+# Frontier class using Stack (LIFO) for DFS
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        # Check if state already in frontier
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        # Remove node from end (stack behavior)
+        if self.empty():
+            raise Exception("empty frontier")
+        node = self.frontier[-1]
+        self.frontier = self.frontier[:-1]
+        return node
+
+# Frontier class using Queue (FIFO) for BFS
+m.output_image("maze.png", show_explored=True)
